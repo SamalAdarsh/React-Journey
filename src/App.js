cfb355d83232d24ab164edf5a59679a413f9c943
Adarsh <codewithadarsh007@gmail.com>
2025-12-24 13:04:59 +0530

    Lazy Loading/Dynamic Bundling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import RestaurentMenu from "./components/RestaurentMenu";
+// import Grocery from "./components/Grocery";
 
 // const styles = {
 
@@ -108,11 +109,15 @@ const resObj = {
   widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
 };
 
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About  = lazy(()=>import("./components/About"))
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Outlet/>
+      <Outlet />
     </div>
   );
 };
@@ -137,7 +142,16 @@ const appRouter = createBrowserRouter([
         element: <Contact />,
       },
 
-       {
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading......</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+
+      {
         path: "/restaurents/:resId",
         element: <RestaurentMenu />,
       },
---
+++ src/components/Grocery.js
@@ -0,0 +1,16 @@
+
+const Grocery = ()=>{
+
+    return(
+        <div className="grocery">
+            <h1>{""}
+
+                This is the grocery instamart page......
+
+            </h1>
+        </div>
+
+    );
+}
+
+export default Grocery;
